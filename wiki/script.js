15bf4659817d7a6991b735e065c6bbd64bf19d78
Karyll Catubig <kjcatubig@massart.edu>
2015-11-02 13:05:25 -0500

    asd

--- wiki/script.js
+++ wiki/script.js
@@ -1,12 +1,8 @@
-$(window).scroll(function(){
-    if($(document).scrollTop() > 600 && $(document).scrollTop() < 2500){
-         var temp =  100 - ($(document).scrollTop() - 1200) / 8;
-         var bonus = '50% ' + temp*-1 + '%';
-         document.getElementById('div').style.backgroundPosition = bonus;
-    }
-}
-
 $(".scroller").scrollFlight();
-$(".title").on("arrived",function() {
+$("#title").on("arrived",function() {
   $(this).addClass("arrived");
+});
+
+$(".image-container").on("click",function() {
+  alert('wtf');
 });
