9c9118a4b1f5c844342edbef06292e6c407a33c4
Karyll Catubig <kjcatubig@massart.edu>
2015-11-16 03:29:04 -0500

    ye

--- hw4/script.js
+++ hw4/script.js
@@ -1,8 +1,48 @@
 $(".scroller").scrollFlight();
-$("#title").on("arrived",function() {
-  $(this).addClass("arrived");
+$("h4").on("arrived",function() {
+  $(this).addClass("arrives");
 });
 
 $(".image-container").on("click",function() {
   alert('wtf');
 });
+
+$(".dash").on("arrived", function(e) {
+  $(this).addClass("arrived");
+  return false;
+});
+
+$(".dash").on("rearrived", function(e) {
+  $(this).removeClass("arrived");
+  return false;
+});
+
+  $(".image-container").on("arrived", function(e) {
+    $(this).addClass("arrived");
+    return false;
+  });
+
+  $(".image-container").on("rearrived", function(e) {
+    $(this).removeClass("arrived");
+    return false;
+  });
+
+  $(".image-container2").on("arrived", function(e) {
+    $(this).addClass("arrived");
+    return false;
+  });
+
+  $(".image-container2").on("rearrived", function(e) {
+    $(this).removeClass("arrived");
+    return false;
+  });
+
+  $(".image-container3").on("arrived", function(e) {
+    $(this).addClass("arrived");
+    return false;
+  });
+
+  $(".image-container3").on("rearrived", function(e) {
+    $(this).removeClass("arrived");
+    return false;
+  });
